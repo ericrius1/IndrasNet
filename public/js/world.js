8e283fa8e85df9ae5d7a78f42b9b486d8939aaff
Eric <Ericrius1@gmail.com>
2013-06-20 16:35:57 -0700

    camera fly

--- public/js/world.js
+++ public/js/world.js
@@ -1,5 +1,5 @@
 var World = function() {
-  var camera, cubeCamera, scene, renderer;
+  var camera, cubeCamera, scene, renderer, controls;
   var fov = 70,
     isUserInteracting = false,
     onMouseDownMouseX = 0,
@@ -10,6 +10,8 @@ var World = function() {
     onMouseDownLat = 0,
     phi = 0,
     theta = 0;
+  var clock = new THREE.Clock();
+  var flyMode = true;
 
   var texture = THREE.ImageUtils.loadTexture('images/house.jpg', new THREE.UVMapping(), function() {
 
@@ -41,22 +43,33 @@ var World = function() {
     document.body.appendChild(renderer.domElement);
 
     //LIGHTS
-    var light1 = new THREE.AmbientLight( 0xffffff, 50, 50 );
-    scene.add( light1 );
+    var light1 = new THREE.AmbientLight(0xffffff, 50, 50);
+    scene.add(light1);
     light1.position = new THREE.Vector3(10, 10, 10);
 
-    var light2 = new THREE.PointLight( 0xff00ff, 20, 50 );
-    scene.add( light2 );
+    var light2 = new THREE.PointLight(0xff00ff, 20, 50);
+    scene.add(light2);
     light2.position = new THREE.Vector3(10, 10, 10);
 
+    //CONTROLS
+    controls = new THREE.FirstPersonControls(camera);
 
-  
+    controls.movementSpeed = 250;
+    controls.lookSpeed = 0.2;
+    controls.lookVertical = true;
+
+    controls.lon = -90;
+
 
     createSpheres();
+    if (!flyMode) {
+      debugger;
+      document.addEventListener('mousedown', onDocumentMouseDown, false);
+      document.addEventListener('mousewheel', onDocumentMouseWheel, false);
+      document.addEventListener('DOMMouseScroll', onDocumentMouseWheel, false);
+    }
 
-    document.addEventListener('mousedown', onDocumentMouseDown, false);
-    document.addEventListener('mousewheel', onDocumentMouseWheel, false);
-    document.addEventListener('DOMMouseScroll', onDocumentMouseWheel, false);
+
     window.addEventListener('resize', onWindowResized, false);
 
     onWindowResized(null);
@@ -70,7 +83,6 @@ var World = function() {
   }
 
   function onDocumentMouseDown(event) {
-
     event.preventDefault();
 
     onPointerDownPointerX = event.clientX;
@@ -85,14 +97,12 @@ var World = function() {
   }
 
   function onDocumentMouseMove(event) {
-
     lon = (event.clientX - onPointerDownPointerX) * 0.1 + onPointerDownLon;
     lat = (event.clientY - onPointerDownPointerY) * 0.1 + onPointerDownLat;
 
   }
 
   function onDocumentMouseUp(event) {
-
     document.removeEventListener('mousemove', onDocumentMouseMove, false);
     document.removeEventListener('mouseup', onDocumentMouseUp, false);
 
@@ -136,23 +146,15 @@ var World = function() {
     var time = Date.now();
 
     // lon += .15;
-
     lat = Math.max(-85, Math.min(85, lat));
     phi = THREE.Math.degToRad(90 - lat);
     theta = THREE.Math.degToRad(lon);
 
-    // sphere1.position.x = Math.sin(time * 0.001) * 30;
-    // sphere1.position.y = Math.sin(time * 0.0011) * 30;
-    // sphere1.position.z = Math.sin(time * 0.0012) * 30;
+    // camera.position.x = 100 * Math.sin(phi) * Math.cos(theta);
+    // camera.position.y = 100 * Math.cos(phi);
+    // camera.position.z = 100 * Math.sin(phi) * Math.sin(theta);
 
-
-   
-
-    camera.position.x = 100 * Math.sin(phi) * Math.cos(theta);
-    camera.position.y = 100 * Math.cos(phi);
-    camera.position.z = 100 * Math.sin(phi) * Math.sin(theta);
-
-    camera.lookAt(scene.position);
+    //camera.lookAt(scene.position);
 
 
     // sphere1.visible = false; // *cough*
@@ -160,26 +162,27 @@ var World = function() {
     cubeCamera.updateCubeMap(renderer, scene);
 
     // sphere1.visible = true; // *cough*
-
+    controls.update(clock.getDelta());
     renderer.render(scene, camera);
 
   }
 
-  function createSpheres(){
+  function createSpheres() {
     var cubeTarget = cubeCamera.renderTarget;
-    var shinyMaterial = new THREE.MeshPhongMaterial( { color: 0xffffff, ambient: 0xffffff, envMap: cubeTarget  } );
+    var shinyMaterial = new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      ambient: 0xffffff,
+      envMap: cubeTarget
+    });
     var sphereRadius = 20;
     var spacing = sphereRadius * 3;
     var sphere;
     var i = 1;
-    for(var x = -20; x <400; x+=spacing){
+    for (var x = -20; x < 400; x += spacing) {
       sphere = new THREE.Mesh(new THREE.SphereGeometry(sphereRadius, 30, 15 * i), shinyMaterial);
       sphere.position.x = x;
       scene.add(sphere);
     }
-    // sphere1 = new THREE.Mesh(new THREE.SphereGeometry(sphereRadius, 30, 15), shinyMaterial);
-    // scene.add(sphere1);
-
   }
 
 }
